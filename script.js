6627b78cc0ee4cadc16d0c0f6b46d6056398eb7a
Shoaib <shoaibsarwar187@gmail.com>
2023-11-20 12:41:02 +0100

    table in JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+// Table Find By User Given NUmber
+
+let table = prompt("Please enter the table you want to see: ");
+
+
+for(let i=1; i<=10; i++){
+    console.log(table + "*" + i + "=" +table * i )
+}
