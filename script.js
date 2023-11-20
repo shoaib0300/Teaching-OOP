0ac49fd72714a1a8a3d72efaa5bf8c5a71418135
Shoaib <shoaibsarwar187@gmail.com>
2023-11-20 13:06:06 +0100

    Print Stars

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+// Print stars in JavaScript
+
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= i; j++) {
+        // Print '*' character
+        console.log("*");
+    }
+    // Move to the next line after each row
+    console.log("\n");
+}
+  
 
 
 // How to find limited numbers even and Odd
 
-let number = prompt("Please enter how many numbers you want to check for even and odd: ");
+// let number = prompt("Please enter how many numbers you want to check for even and odd: ");
 
-for (let i = 1; i <= number; i++) {
-    if (i % 2 !== 0) {
-        console.log(i + " is odd");
-    } else {
-        console.log(i + " is even");
-    }
-}
+// for (let i = 1; i <= number; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i + " is odd");
+//     } else {
+//         console.log(i + " is even");
+//     }
+// }
 
 
 
