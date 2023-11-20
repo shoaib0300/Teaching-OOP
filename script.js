bff8b14b3e9f5e1790ce6a596933ba904408b386
Shoaib <shoaibsarwar187@gmail.com>
2023-11-20 12:58:51 +0100

    Even And Odd Number

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 
 
+// How to find limited numbers even and Odd
+
+let number = prompt("Please enter how many numbers you want to check for even and odd: ");
+
+for (let i = 1; i <= number; i++) {
+    if (i % 2 !== 0) {
+        console.log(i + " is odd");
+    } else {
+        console.log(i + " is even");
+    }
+}
+
+
+
 // Table Find By User Given NUmber
 
-let table = prompt("Please enter the table you want to see: ");
+//let table = prompt("Please enter the table you want to see: ");
 
 
-for(let i=1; i<=10; i++){
-    console.log(table + "*" + i + "=" +table * i )
-}
+// for(let i=1; i<=10; i++){
+//     console.log(table + "*" + i + "=" +table * i )
+// }
